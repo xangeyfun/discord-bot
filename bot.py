f375ed930f782b8399862a1a1362eb8c0776c3f7
xangeyfun <kutkindje101@gmail.com>
2025-12-02 09:25:18 +0100

    first commands

--- bot.py
+++ bot.py
@@ -6,11 +6,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-client = discord.Client(intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
 
-@client.event
-async def on_ready() -> None:
-	print(f"Logged in as {client.user}")
-	print(f"Token: {TOKEN}")
+
+@bot.event
+async def on_message(message: discord.Message) -> None:
+    if message.author.bot:
+        return
+
+    if message.content == "Hello":
+        await message.channel.send("Hi")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def ping(ctx: commands.Context) -> None:
+    await ctx.send(f"> Pong! {round(bot.latency * 1000)}ms")
 
 client.run(TOKEN)
